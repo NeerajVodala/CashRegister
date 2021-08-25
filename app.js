318ac8a0346ba728df33b8da69ea8e1c619d7900
NeerajVodala <vodalaneeraj@gmail.com>
2021-08-25 11:35:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-btn");
 const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelector(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 checkButton.addEventListener("click", function onClick() {
@@ -25,6 +25,6 @@ function notesToBeGiven(amountRemaining) {
     for (let i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(amountRemaining / availableNotes[i]);
         amountRemaining = amountRemaining % availableNotes[i];
-        noOfNotes[0].innerText = numberOfNotes;
+        noOfNotes[i].innerText = numberOfNotes;
     }
 }
