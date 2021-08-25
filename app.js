199d78724f2dd7fc25d8c53191c83c3f93136e7a
NeerajVodala <vodalaneeraj@gmail.com>
2021-08-25 16:17:34 +0530

    fixed: finally

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 checkButton.addEventListener("click", function onClick() {
     hideMessage()
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountRemaining = cashGiven.value - billAmount.value;
+    if (Number(billAmount.value) > 0) {
+        if (Number(cashGiven.value) >= Number(billAmount.value)) {
+            const amountRemaining = Number(cashGiven.value) - Number(billAmount.value);
             notesToBeGiven(amountRemaining);
-            console.log("clicked")
+            
         } else {
             showMessage("Are you kidding me? Do you want to do dishes?");
         }
